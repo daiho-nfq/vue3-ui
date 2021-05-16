c831cb395996aecbbbbe34e9bc118a3b7f8612bc
daiho-nfq <dai.ho@nfq.asia>
2021-05-16 20:45:20 +0700

    Create customer profile form

--- src/components/Customer/CustomerTab/CustomerTabProfile.vue
+++ src/components/Customer/CustomerTab/CustomerTabProfile.vue
@@ -1,5 +1,27 @@
 <template>
-    <div>Tab Profile</div>
+    <div class="grid grid-cols-6 sm:gap-4">
+        <app-p5 class="col-span-6"> Personal Info </app-p5>
+        <form-group class="col-span-6 sm:col-span-2">
+            <form-label :label="'Full Name'" />
+            <form-input :placeholder="'Full Name'" />
+        </form-group>
+        <form-group class="col-span-6 sm:col-span-2">
+            <form-label :label="'Email'" />
+            <form-input :placeholder="'Email'" :type="'email'" />
+        </form-group>
+        <form-group class="col-span-6 sm:col-span-2">
+            <form-label :label="'Phone Number'" />
+            <form-input :placeholder="'Phone number'" />
+        </form-group>
+        <form-group class="col-span-6 sm:col-span-4">
+            <form-label :label="'Address'" />
+            <form-input :placeholder="'Address'" />
+        </form-group>
+        <form-group class="col-span-6 sm:col-span-2">
+            <form-label :label="'City'" />
+            <form-input :placeholder="'City'" />
+        </form-group>
+    </div>
 </template>
 
 <script>
--- src/components/Form/FormLabel.vue
+++ src/components/Form/FormLabel.vue
@@ -1,11 +1,15 @@
 <template>
-    <app-p2 class="truncate">
-        <slot />
-    </app-p2>
+    <app-p2 class="truncate font-medium text-gray-700" v-text="label" />
 </template>
 
 <script>
 export default {
     name: "FormLabel",
+    props: {
+        label: {
+            type: String,
+            required: true,
+        },
+    },
 };
 </script>
