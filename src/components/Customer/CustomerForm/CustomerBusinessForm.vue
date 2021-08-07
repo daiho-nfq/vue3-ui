cc1ead952e0764c4b4ad121ee8851a3ff29395e5
htdai <dai.ho@nfq.asia>
2021-08-08 02:29:18 +0700

    Update customer business form

---
+++ src/components/Customer/CustomerForm/CustomerBusinessForm.vue
@@ -0,0 +1,60 @@
+<template>
+    <customer-form :form-data="formData" :is-disable-submit="isDisableSubmit">
+        <form-group class="col-span-6 sm:col-span-3">
+            <form-label :label="'Company Name'" />
+            <form-input v-model="company" :placeholder="'Company Name'" />
+        </form-group>
+        <form-group class="col-span-6 sm:col-span-3">
+            <form-label :label="'Company Email'" />
+            <form-input v-model="email" :placeholder="'Company Email'" />
+        </form-group>
+        <form-group class="col-span-6 sm:col-span-3">
+            <form-label :label="'Company Address'" />
+            <form-input v-model="address" :placeholder="'Company Address'" />
+        </form-group>
+        <form-group class="col-span-6 sm:col-span-3">
+            <form-label :label="'Company Tax Identification Number'" />
+            <form-input v-model="taxIdentificationNumber" :placeholder="'Company Tax Identification Number'" />
+        </form-group>
+    </customer-form>
+</template>
+
+<script>
+import { isValueEmpty } from "@/helpers/utils";
+import CustomerForm from "@/components/Customer/CustomerForm.vue";
+export default {
+    name: "CustomerBusinessForm",
+    components: {
+        CustomerForm,
+    },
+    props: {
+        business: {
+            type: Object,
+            required: true,
+        },
+    },
+    data() {
+        return {
+            company: this.business.company,
+            email: this.business.email,
+            address: this.business.address,
+            taxIdentificationNumber: this.business.tax_identification_number,
+        };
+    },
+    computed: {
+        isDisableSubmit() {
+            return (
+                isValueEmpty(this.company) || isValueEmpty(this.address) || isValueEmpty(this.taxIdentificationNumber)
+            );
+        },
+        formData() {
+            return {
+                company: this.company,
+                email: this.email,
+                address: this.address,
+                taxIdentificationNumber: this.taxIdentificationNumber,
+            };
+        },
+    },
+};
+</script>
--- src/components/Customer/CustomerTab/CustomerTabBusiness/CustomerTabBusiness.vue
+++ src/components/Customer/CustomerTab/CustomerTabBusiness/CustomerTabBusiness.vue
@@ -1,22 +1,7 @@
 <template>
-    <div v-if="business" class="grid grid-cols-6 gap-4">
+    <div v-if="business" class="space-y-4">
         <app-h1 class="border-l-4 border-red-500 pl-2 font-medium col-span-6" v-text="'Business'" />
-        <form-group class="col-span-6 sm:col-span-3">
-            <form-label :label="'Company Name'" />
-            <form-input v-model="company" :placeholder="'Company Name'" />
-        </form-group>
-        <form-group class="col-span-6 sm:col-span-3">
-            <form-label :label="'Company Email'" />
-            <form-input v-model="email" :placeholder="'Company Email'" />
-        </form-group>
-        <form-group class="col-span-6 sm:col-span-3">
-            <form-label :label="'Company Address'" />
-            <form-input v-model="address" :placeholder="'Company Address'" />
-        </form-group>
-        <form-group class="col-span-6 sm:col-span-3">
-            <form-label :label="'Company Tax Identification Number'" />
-            <form-input v-model="taxIdentificationNumber" :placeholder="'Company Tax Identification Number'" />
-        </form-group>
+        <customer-business-form :business="business" />
     </div>
 </template>
 
@@ -24,8 +9,12 @@
 import { mapGetters } from "vuex";
 import { isValueEmpty, getValueByPath } from "@/helpers/utils";
 import { getter_customer_business } from "@/store/getters";
+import CustomerBusinessForm from "@/components/Customer/CustomerForm/CustomerBusinessForm.vue";
 export default {
     name: "CustomerTabBusiness",
+    components: {
+        CustomerBusinessForm,
+    },
     data() {
         return {
             company: "",
